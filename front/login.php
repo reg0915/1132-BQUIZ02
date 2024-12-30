1884545adc50896f2e329ddf1d2406db938a58dd
reg <reg8740223@gmail.com>
2024-12-30 14:17:09 +0800

    建立會員登入頁面

---
+++ front/login.php
@@ -0,0 +1,19 @@
+<fieldset>
+    <legend>會員登錄</legend>
+    <table>
+        <tr>
+            <td>帳號管理</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" value="登入" id=""><input type="reset" value="清除"></td>
+            <a href="?do=forget">忘記密碼</a>
+            <a href="?do=reg">尚未註冊</a>
+        </tr>
+    </table>
+</fieldset>
