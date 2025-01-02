f28dc90949f182cb5ae6295e841290abf15d2f76
reg <reg8740223@gmail.com>
2025-01-02 14:37:45 +0800

    完成文章管理功能,使用ajax

--- api/db.php
+++ api/db.php
@@ -163,6 +163,7 @@ function to($url){
 
 $Total=new DB('total');
 $User=new DB('users');
+$News=new DB('news');
 
 
 if(!isset($_SESSION['view'])){
--- back/acc.php
+++ back/acc.php
@@ -1,15 +1,14 @@
 <fieldset style="width:75%;margin:auto;">
     <legend>帳號管理</legend>
-    <table>
+    <table class='ct' style="width:75%;margin:auto;">
         <tr>
             <td>帳號</td>
             <td>密碼</td>
             <td>刪除</td>
         </tr>
-        <?php
+        <?php 
         $rows=$User->all();
-        foreach($rows as $row):
-        ?>
+        foreach($rows as $row):?>
         <tr>
             <td><?=$row['acc'];?></td>
             <td><?=str_repeat("*",strlen($row['pw']));?></td>
@@ -19,59 +18,49 @@
         </tr>
         <?php endforeach;?>
     </table>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <div class="ct"><button onclick='del()'>確定刪除</button><button onclick='resetChk()'>清空選取</button></div>
     <h2>新增會員</h2>
-    <fieldset style="width:60%;margin:auto;">
-        <legend>會員註冊</legend>
-        <div style="color:red">
-            *請設定您要註冊的帳號及密碼(最常12個字元)
-        </div>
-        <table>
-            <tr>
-                <td>Step:1登入帳號</td>
-                <td><input type="text" name="acc" id="acc"></td>
-            </tr>
-            <tr>
-                <td>Step:2登入密碼</td>
-                <td><input type="password" name="pw" id="pw"></td>
-            </tr>
-            <tr>
-                <td>Step:3再次確認密碼</td>
-                <td><input type="password" name="pw2" id="pw2"></td>
-            </tr>
-            <tr>
-                <td>Step:4信箱(忘記密碼時使用)</td>
-                <td><input type="text" name="emaol" id="email"></td>
-            </tr>
-            <tr>
-
-                <td>
-                    <input type="button" value="註冊" onclick='reg()'>
-                    <input type="button" value="清除" onclick='restform()'>
-                </td>
-
-            </tr>
-        </table>
-    </fieldset>
-
+    <div style="color:red">
+        *請設定您要註冊的帳號及密碼(最長12個字元)
+    </div>
+    <table>
+        <tr>
+            <td>Step1:登入帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>Step2:登入密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td>Step3:再次確認密碼</td>
+            <td><input type="password" name="pw2" id="pw2"></td>
+        </tr>
+        <tr>
+            <td>Step4:信箱(忘記密碼時使用)</td>
+            <td><input type="text" name="email" id="email"></td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" value="註冊" onclick='reg()'>
+                <input type="button" value="清除" onclick='resetForm()'>
+            </td>
+            <td></td>
+        </tr>
+    </table>
     <script>
     function del() {
-        let dels = $("input[name='del'[];checked");
+        let dels = $("input[name='del[]']:checked");
         let ids = new Array();
-        dels.each(idx)
+        dels.each((idx, item) => {
+            ids.push($(item).val())
+        })
+        /* console.log(ids) */
+        $.post('./api/del_user.php', {
+            ids
+        }, () => {
+            location.reload();
+        })
     }
 
     function reg() {
@@ -79,9 +68,7 @@
             acc: $("#acc").val(),
             pw: $("#pw").val(),
             pw2: $("#pw2").val(),
-            email: $("#email").val(),
-
-
+            email: $("#email").val()
         }
         if (user.acc == "" || user.pw == "" || user.pw2 == "" || user.email == "") {
             alert("不可空白");
@@ -91,32 +78,31 @@
             $.get("./api/chk_acc.php", {
                 acc: user.acc
             }, (res) => {
-                // console.log("chk acc => ", res)
+                //console.log("chk acc => ",res)
                 if (parseInt(res) > 0) {
                     alert("帳號重複")
                 } else {
                     $.post("./api/reg.php", user, (res) => {
-                        // console.log("reg => ", res)
-                        // if (parseInt(res) == 1) {
-                        //     alert("註冊完成，歡迎加入")
-                        // }
+                        location.reload();
+                        //console.log("reg => ",res)
+                        /* if(parseInt(res)==1){
+                            alert("註冊完成，歡迎加入")
+                        } */
                     })
                 }
             })
         }
     }
 
-    function restForm() {
-        let user = {
-            acc: $("#acc").val(''),
-            pw: $("#pw").val(''),
-            pw2: $("#pw2").val(''),
-            email: $("#email").val(''),
-        }
+    function resetForm() {
+        $("#acc").val("")
+        $("#pw").val("")
+        $("#pw2").val("")
+        $("#email").val("")
+    }
 
-        function resetChk() {
-            $("input[type='checkbox']").prop("checked", false)
-        }
+    function resetChk() {
+        $("input[type='checkbox']").prop("checked", false)
     }
     </script>
 </fieldset>
